a437582a8ed08fbf92393881ddebbb75ba0ef783
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-04-03 20:26:07 +0200

    Set loading gif speed as int

--- main.py
+++ main.py
@@ -853,17 +853,12 @@ def identicon(username):
 @app.route("/api/loading.gif")
 @excluded
 def generate_gif():
-    speed = request.args.get("speed")
-
-    if speed == "quick":
-        duration = 350
-    else:
-        duration = 670
+    speed = int(request.args.get("speed", 670))
 
     images = [Image.open(io.BytesIO(render_identicon("".join(random.choices(string.ascii_letters + string.digits, k=random.randint(5, 20))), (0, 0, 0, 0)))) for _ in range(20)]
 
     gif_bytes = io.BytesIO()
-    images[0].save(gif_bytes, format="GIF", save_all=True, append_images=images[1:], duration=duration, loop=0, disposal=2)
+    images[0].save(gif_bytes, format="GIF", save_all=True, append_images=images[1:], duration=speed, loop=0, disposal=2)
     gif_bytes.seek(0)
 
     return Response(gif_bytes.read(), mimetype="image/gif")
